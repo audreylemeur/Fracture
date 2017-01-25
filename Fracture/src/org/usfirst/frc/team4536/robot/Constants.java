74fec6627c9c9b806aa92f9e24fc632ab845019c
Audrey <alemeur001@stpaul.k12.mn.us>
2017-01-24 18:38:57 -0600

    constants for new joysticks

--- Fracture/src/org/usfirst/frc/team4536/robot/Constants.java
+++ Fracture/src/org/usfirst/frc/team4536/robot/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
   
 /*-----------------------------------------------------------Utilities----------------------------------------------------------*/
 	
-	public static final double DEAD_ZONE = 0.5;
+	public static final double DEAD_ZONE = 0.05;
 	public static final double FORWARD_SPEED_CURVE = 0;
 	public static final double TURN_SPEED_CURVE = 0;
 	public static final double STRAFE_SPEED_CURVE = 0;
