35fabe129ba7d8c29f25b0503d8310e24906e704
TechnoArtist <sheilabrittany@gmail.com>
2017-01-26 19:12:37 -0600

    added Constants to camera

--- Fracture/src/org/usfirst/frc/team4536/robot/Constants.java
+++ Fracture/src/org/usfirst/frc/team4536/robot/Constants.java
@@ -8,5 +8,7 @@ public class Constants {
 	public static final double FORWARD_SPEED_CURVE = 0;
 	public static final double TURN_SPEED_CURVE = 0;
 	public static final double STRAFE_SPEED_CURVE = 0;
-
+	public static final int CAMERA_RESOLUTION_WIDTH = 1280;
+	public static final int CAMERA_RESOLUTION_HEIGHT = 780;
 }
+
