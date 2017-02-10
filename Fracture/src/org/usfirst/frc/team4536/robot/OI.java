a4eb7d4f638669093ad30d50cda90293ce1dced1
Audrey <alemeur001@stpaul.k12.mn.us>
2017-02-09 19:45:17 -0600

    Added button for backup drive

--- Fracture/src/org/usfirst/frc/team4536/robot/OI.java
+++ Fracture/src/org/usfirst/frc/team4536/robot/OI.java
@@ -23,7 +23,6 @@ public class OI {
 	public static Button holdMiddle;
 	public static Button holdRight;
 	public static Button fieldCentric;
-	public static Button autoRotate;
 	
 	public static Button plusDegree;
 	public static Button minusDegree;
@@ -31,10 +30,12 @@ public class OI {
 	public static double feederStationAngle;
 	public static Button switchSao;
 	public static Button switchPrimary;
+	public static Button switchBackup;
 	public static Button climb;
 	
 	public static Button fullSpeedClimb;
 	public static Button slowClimb;
+
 
 	
 	public static void ButtonHandling() {
@@ -43,57 +44,53 @@ public class OI {
 		holdMiddle = new JoystickButton(primaryRightStick, RobotMap.HOLD_MIDDLE_BUTTON);
 		holdRight = new JoystickButton(primaryRightStick, RobotMap.HOLD_RIGHT_BUTTON);
 		fieldCentric = new JoystickButton(primaryRightStick, RobotMap.HOLD_CENTER_BUTTON);
-		autoRotate = new JoystickButton(primaryRightStick, RobotMap.HOLD_SPECIAL_BUTTON);
 
-		plusDegree = new JoystickButton(primaryLeftStick, RobotMap.PLUS_DEGREE_BUTTON);
-		minusDegree = new JoystickButton(primaryLeftStick, RobotMap.MINUS_DEGREE_BUTTON);
+		plusDegree = new JoystickButton(primaryRightStick, RobotMap.PLUS_DEGREE_BUTTON);
+		minusDegree = new JoystickButton(primaryRightStick, RobotMap.MINUS_DEGREE_BUTTON);
 
+		setFeederStationAngle();
 		holdFeeder.whenPressed(new HoldAngle(feederStationAngle));
 		
-    switchSao = new JoystickButton(secondaryStick, RobotMap.SAO_SWITCH);
+		switchSao = new JoystickButton(secondaryStick, RobotMap.SAO_SWITCH);
+		switchPrimary = new JoystickButton(primaryRightStick, RobotMap.PRIMARY_SWITCH);
+		switchBackup = new JoystickButton(primaryLeftStick, RobotMap.BACKUP_SWITCH); 
+		
     	fullSpeedClimb = new JoystickButton(secondaryStick, RobotMap.FULL_CLIMB);
 		slowClimb = new JoystickButton(secondaryStick, RobotMap.SLOW_CLIMB);
-		switchPrimary = new JoystickButton(primaryRightStick, RobotMap.PRIMARY_SWITCH);
 		climb = new JoystickButton(secondaryStick, RobotMap.CLIMB);
 		
 		holdLeft.whenPressed(new HoldAngle(Constants.LEFT_PEG_ANGLE));
 		holdMiddle.whenPressed(new HoldAngle(Constants.MIDDLE_PEG_ANGLE));
 		holdRight.whenPressed(new HoldAngle(Constants.RIGHT_PEG_ANGLE));
 		fieldCentric.whenPressed(new HoldAngle(Constants.RIGHT_PEG_ANGLE));
-		autoRotate.whenPressed(new AutoRotateDriveHoldAngle(Constants.RIGHT_PEG_ANGLE));
 		fullSpeedClimb.whenPressed(new RunClimber(1));
 		slowClimb.whenPressed(new RunClimber(Constants.SLOW_CLIMB_SPEED));
 
-		switchSao = new JoystickButton(secondaryStick, RobotMap.SAO_SWITCH);
-		switchPrimary = new JoystickButton(primaryRightStick, RobotMap.PRIMARY_SWITCH);
 		climb = new JoystickButton(secondaryStick, RobotMap.CLIMB);
 		
 		plusDegree.whenPressed(new AngleAdjustment(true));
 		minusDegree.whenPressed(new AngleAdjustment(false));
 		
-		holdFeeder.whenReleased(new Drive());
-		holdLeft.whenReleased(new Drive());
-		holdMiddle.whenReleased(new Drive());
-		holdRight.whenReleased(new Drive());
-		fieldCentric.whenReleased(new Drive());
-		autoRotate.whenReleased(new Drive());
+		holdFeeder.whenReleased(new AutoRotateFieldCentric());
+		holdLeft.whenReleased(new AutoRotateFieldCentric());
+		holdMiddle.whenReleased(new AutoRotateFieldCentric());
+		holdRight.whenReleased(new AutoRotateFieldCentric());
+		fieldCentric.whenReleased(new AutoRotateFieldCentric());
 		fullSpeedClimb.whenReleased(new RunClimber(0));
 		slowClimb.whenReleased(new RunClimber(0));
 
 		switchSao.whenPressed(new SaoDrive());
-		switchPrimary.whenPressed(new Drive());
+		switchPrimary.whenPressed(new AutoRotateFieldCentric());
+		switchBackup.whenPressed(new BackupDrive());
 		
 	}
 	
-	public void setFeederStationAngle(){
+	public static void setFeederStationAngle(){
 		if ((DriverStation.getInstance()).getAlliance() == DriverStation.Alliance.Blue) {
-			feederStationAngle = 116.6;
+			feederStationAngle = Constants.FEEDER_STATION_ANGLE;
 		}
 		else if ((DriverStation.getInstance()).getAlliance() == DriverStation.Alliance.Red) {
-			feederStationAngle = -116.6;	
-		}
-		else {
-			feederStationAngle = 0.0;
+			feederStationAngle = -Constants.FEEDER_STATION_ANGLE;	
 		}
 	}
 }
