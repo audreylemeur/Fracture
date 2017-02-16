9c955b87c348fa32a8bebd44b607326c2be7cc7b
Theo Hamilton <theohamilton@Theos-MacBook-Pro.local>
2017-02-16 16:04:02 -0600

    making autoHoldAngle

---
+++ Fracture/src/org/usfirst/frc/team4536/robot/commands/AutoHoldAngle.java
@@ -0,0 +1,64 @@
+package org.usfirst.frc.team4536.robot.commands;
+
+import org.usfirst.frc.team4536.robot.OI;
+import org.usfirst.frc.team4536.utilities.Constants;
+import org.usfirst.frc.team4536.utilities.NavXException;
+import org.usfirst.frc.team4536.utilities.Utilities;
+
+/**
+ *@author Theo
+ *Class to drive while holding an angle, field-centric.
+ */
+public class AutoHoldAngle extends CommandBase {
+	private double forwardThrottle, strafeThrottle, turnThrottle, dAng;
+
+    public AutoHoldAngle(double desiredEndAngle, double desiredAngle, double throttle, double time) {
+        requires(driveTrain);
+        dAng = desiredAngle;
+    }
+
+    protected void initialize() {
+    	forwardThrottle = 0;
+    	strafeThrottle = 0;
+    	turnThrottle = 0;
+    	//Keep the robot from spazzing out.
+    }
+
+    protected void execute() {
+    	
+    	try {
+    		
+    		double speedCurveMagnitude = Utilities.speedCurve(OI.primaryRightStick.getModMagnitude(), Constants.FIELD_SPEED_CURVE);
+    		forwardThrottle = Math.cos(Math.toRadians(driveTrain.getNavX().getAngle() - OI.primaryRightStick.getDirectionDegrees())) * speedCurveMagnitude;
+        	strafeThrottle = Math.sin(Math.toRadians(driveTrain.getNavX().getAngle() - OI.primaryRightStick.getDirectionDegrees())) * Constants.FORWARD_STRAFE_RATIO * -speedCurveMagnitude;
+
+        	double angleDif = Utilities.angleDifference(driveTrain.getNavX().getAngle(), dAng);
+        	turnThrottle = angleDif * Constants.HOLD_ANGLE_P_CONSTANT;
+        	turnThrottle = Utilities.limit(turnThrottle, 1 - Constants.AUTO_ROTATE_SCALE_PARAM);
+        	forwardThrottle = Utilities.scale(forwardThrottle, strafeThrottle, 1 - Math.abs(turnThrottle));
+        	strafeThrottle = Utilities.scale(strafeThrottle, forwardThrottle, 1 - Math.abs(turnThrottle));
+        		
+    		driveTrain.Drive(forwardThrottle, strafeThrottle, turnThrottle);
+    		
+    	}
+    	catch(NavXException e) {
+    		end();
+    	}
+    	
+    	//driveTrain.setLastDesiredAngle(rAng);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+  }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
