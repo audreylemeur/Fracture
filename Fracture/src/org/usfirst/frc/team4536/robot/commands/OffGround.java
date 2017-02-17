e629495e282381a305568002890191641687eadd
Audrey <alemeur001@stpaul.k12.mn.us>
2017-02-17 15:17:48 -0600

    new Button for automatic climbing

--- Fracture/src/org/usfirst/frc/team4536/robot/commands/OffGround.java
+++ Fracture/src/org/usfirst/frc/team4536/robot/commands/OffGround.java
@@ -1,14 +1,16 @@
 package org.usfirst.frc.team4536.robot.commands;
 
+import org.usfirst.frc.team4536.utilities.EnhancedTimer;
 import org.usfirst.frc.team4536.utilities.NavXException;
 
-import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
 public class OffGround extends CommandBase {
 
+	EnhancedTimer timer;
+	
     public OffGround() {
         
     	requires(climber);
@@ -17,29 +19,42 @@ public class OffGround extends CommandBase {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	timer = new EnhancedTimer();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	try{
+    		
+    		if(driveTrain.getNavX().getPitch()>45){
+            	
+            	timer.startTimer();
+            	climber.setClimber(0);
+            	
+            }else climber.setClimber(1);
+            
+    	}catch(NavXException e) {
+    		
+    		;
+    	}
+    	
     	
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-    	try{
-        if(driveTrain.getNavX().getPitch()>80 && driveTrain.getNavX().getPitch()<-280){
-        	
-        	return true;
-        	
-        }else return false;
-    	}catch(NavXException e) {
+    	
+    	if (timer.getTime()>1){
     		
     		return true;
-    	}
+    		
+    	}else return false;
+    	
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	
     }
 
     // Called when another command which requires one or more of the same
