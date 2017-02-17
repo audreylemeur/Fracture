daaafd3a0d4c9f3aee6df93510f1c322d7bc771c
Audrey <alemeur001@stpaul.k12.mn.us>
2017-02-17 16:44:45 -0600

    Some off ground constants

--- Fracture/src/org/usfirst/frc/team4536/robot/commands/OffGround.java
+++ Fracture/src/org/usfirst/frc/team4536/robot/commands/OffGround.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team4536.robot.commands;
 
+import org.usfirst.frc.team4536.utilities.Constants;
 import org.usfirst.frc.team4536.utilities.EnhancedTimer;
 import org.usfirst.frc.team4536.utilities.NavXException;
 
@@ -26,7 +27,7 @@ public class OffGround extends CommandBase {
     protected void execute() {
     	try{
     		
-    		if(driveTrain.getNavX().getPitch()>45){
+    		if(driveTrain.getNavX().getPitch()>Constants.OFF_GROUND_ANGLE){
             	
             	timer.startTimer();
             	climber.setClimber(0);
@@ -44,7 +45,7 @@ public class OffGround extends CommandBase {
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
     	
-    	if (timer.getTime()>1){
+    	if (timer.getTime()>Constants.OFF_GROUND_TIME){
     		
     		return true;
     		
